a0cbd583284a56540a7e5ff8e1143f58fdc274a5
Jan Midtgaard <mail@janmidtgaard.dk>
2021-10-23 04:09:57 +0200

    filter filename paths

--- test/instrumentation-tests.t/filter_dune_build.sh
+++ test/instrumentation-tests.t/filter_dune_build.sh
@@ -2,4 +2,5 @@
 
 # breakdown:
 #   filter [@@@ocaml.ppx.context whitespace {  0-or-more not-close-brace-chars }]
-dune build $@ 2>&1 | sed  -Ez 's/\[@@@ocaml\.ppx\.context\s+\{[^}]*\}\]//g'
+#   and normalize file system paths
+dune build $@ 2>&1 | sed  -Ez 's/\[@@@ocaml\.ppx\.context\s+\{[^}]*\}\]//g' | sed 's/home[^ ]*bin\//home\/...\/bin\//'
