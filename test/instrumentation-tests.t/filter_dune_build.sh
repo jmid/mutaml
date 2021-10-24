46aa9397816b574c3833abbd57723fab42a477c6
Jan Midtgaard <mail@janmidtgaard.dk>
2021-10-24 10:57:01 +0200

    use POSIX sed options

--- test/instrumentation-tests.t/filter_dune_build.sh
+++ test/instrumentation-tests.t/filter_dune_build.sh
@@ -3,4 +3,6 @@
 # breakdown:
 #   filter [@@@ocaml.ppx.context whitespace {  0-or-more not-close-brace-chars }]
 #   and normalize file system paths
-dune build $@ 2>&1 | sed  -Ez 's/\[@@@ocaml\.ppx\.context\s+\{[^}]*\}\]//g' | sed 's/home[^ ]*bin\//home\/...\/bin\//'
+dune build $@ 2>&1 | \
+    sed  -E 'H;1h;$!d;x;s/\[@@@ocaml\.ppx\.context\s+\{[^}]*\}\]//g' | \
+    sed 's/home[^ ]*bin\//home\/...\/bin\//'
