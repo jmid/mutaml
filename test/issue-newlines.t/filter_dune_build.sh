1b6ed95728ebef147eb56067e3a810c83dc77f7d
Jan Midtgaard <mail@janmidtgaard.dk>
2021-12-04 19:44:45 +0100

    update issue-copy of shell-filter

--- test/issue-newlines.t/filter_dune_build.sh
+++ test/issue-newlines.t/filter_dune_build.sh
@@ -1,8 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # breakdown:
 #   filter [@@@ocaml.ppx.context whitespace {  0-or-more not-close-brace-chars }]
 #   and normalize file system paths
 dune build $@ 2>&1 | \
     sed  -E 'H;1h;$!d;x;s/\[@@@ocaml\.ppx\.context\s+\{[^}]*\}\]//g' | \
-    sed 's/home[^ ]*bin\//home\/...\/bin\//'
+    sed 's/home[^ ]*bin\//some\/path\/...\/bin\//' | \
+    sed 's/Users[^ ]*bin\//some\/path\/...\/bin\//'
