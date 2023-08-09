cfdbf3b621f879554537584c330685011c9cd368
Jan Midtgaard <mail@janmidtgaard.dk>
2023-08-09 22:10:10 +0200

    Update warning filter to delete relocation warning lines on i386

--- test/filter_dune_build.sh
+++ test/filter_dune_build.sh
@@ -4,6 +4,7 @@
 #   filter [@@@ocaml.ppx.context whitespace {  0-or-more not-close-brace-chars }]
 #   and normalize file system paths
 dune build $@ 2>&1 | \
-    sed  -E 'H;1h;$!d;x;s/\[@@@ocaml\.ppx\.context\n\ \ \{[^}]*\}\]//g' | \
+    sed -E 'H;1h;$!d;x;s/\[@@@ocaml\.ppx\.context\n\ \ \{[^}]*\}\]//g' | \
+    sed -E "/^\/usr\/bin\/ld/d" | \
     sed 's/home[^ ]*bin\//some\/path\/...\/bin\//' | \
     sed 's/Users[^ ]*bin\//some\/path\/...\/bin\//'
